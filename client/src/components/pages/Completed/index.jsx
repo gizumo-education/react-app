3d3016b0b4533dbc14f283e1f512f0e638a834c2
taipeijapan <bumpofchelsea0910@gmail.com>
2025-04-15 17:22:57 +0900

    add:ルーティング機能を追加

---
+++ client/src/components/pages/Completed/index.jsx
@@ -0,0 +1,9 @@
+import { Layout } from "../../ui/Layout"
+
+export const Completed = () => {
+  return (
+    <Layout>
+      <h1 className={StyleSheet.heading}>完了済み一覧</h1>
+    </Layout>
+  )
+}
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,10 +4,12 @@ import 'react-toastify/dist/ReactToastify.css'
 import { Top } from './components/pages/Top'
 import './styles/reset.css'
 import './styles/globals.css'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
     <ToastContainer />
-    <Top />
+    <RouterProvider router = {router} />
   </>
 )
