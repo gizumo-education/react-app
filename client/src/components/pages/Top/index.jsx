00c3bb737c5b67bc22b37b3b537f07c215cda52b
haseyama <yamato@yamato.com>
2025-04-17 14:12:57 +0900

    Top useState "タスクを追加"button 追加

--- client/src/components/pages/Top/index.jsx
+++ client/src/components/pages/Top/index.jsx
@@ -2,11 +2,18 @@ import { useState, useEffect } from 'react'
 import { axios } from '../../../utils/axiosConfig'
 import { Layout } from '../../ui/Layout'
 import { ListItem } from '../../ui/ListItem'
+import { Button } from '../../ui/Button'
+import { Icon } from '../../ui/Icon'
 
 import styles from './index.module.css'
 
 export const Top = () => {
   const [todos, setTodos] = useState([])
+
+  const [inputValues, setInputValues] = useState({
+    title: '',
+    description: '',
+  })
 
   useEffect(() => {
     axios.get('http://localhost:3000/todo').then(({ data }) => {
@@ -27,6 +34,17 @@ export const Top = () => {
         {listItems.map((todo) => {
           return <ListItem key={todo.id} todo={todo} />
         })}
+        <li>
+        <Button buttonStyle='indigo-blue' className={styles['add-task']}>
+          <Icon
+            iconName='plus'
+            color='orange'
+            size='medium'
+            className={styles['plus-icon']}
+          />
+          タスクを追加
+        </Button>
+      </li>
       </ul>
     </Layout>
   )
