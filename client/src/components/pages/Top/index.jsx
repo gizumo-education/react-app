15eb096c5743a04a2f1fd5fd8cece94866d4f425
haseyama <yamato@yamato.com>
2025-04-23 16:35:24 +0900

    deleted後のdataをTodoStateに反映

--- client/src/components/pages/Top/index.jsx
+++ client/src/components/pages/Top/index.jsx
@@ -81,7 +81,9 @@ export const Top = () => {
   )
 
   const handleDeleteButtonClick = useCallback((id) => {
-    axios.delete(`http://localhost:3000/todo/${id}`)
+    axios.delete(`http://localhost:3000/todo/${id}`).then(({data}) => {
+      setTodos(data)
+    })
   }, [])
 
     useEffect(() => {
