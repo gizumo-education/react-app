6e7bfd541035b3812130ee5758838ec06317ea6c
haseyama <yamato@yamato.com>
2025-04-23 12:18:02 +0900

    ListItem component editボタン追加

--- client/src/components/ui/ListItem/index.jsx
+++ client/src/components/ui/ListItem/index.jsx
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types'
 import { memo } from 'react'
-
+import { Button } from '../Button'
+import { Icon } from '../Icon'
 import styles from './index.module.css'
 
-export const ListItem = memo(({ todo }) => {
+export const ListItem = memo(({ todo, onEditButtonClick }) => {
 
   return (
     <li className={styles['list-item']}>
@@ -12,6 +13,14 @@ export const ListItem = memo(({ todo }) => {
         {todo.description && (
           <div className={styles.description}>{todo.description}</div>
         )}
+      </div>
+      <div className={styles['task-action']}>
+        <Button
+          buttonStyle='icon-only'
+          onClick={() => onEditButtonClick(todo.id)}
+        >
+          <Icon iconName='edit' color='indigo-blue' size='medium' />
+        </Button>
       </div>
     </li>
   )
@@ -25,4 +34,5 @@ ListItem.propTypes = {
     description: PropTypes.string,
     isCompleted: PropTypes.bool.isRequired,
   }).isRequired,
+  onEditButtonClick: PropTypes.func.isRequired,
 }
