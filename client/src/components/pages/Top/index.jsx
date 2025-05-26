67698e4d1478ae9e7d61b320037f105805481aee
高橋　輝 <hikaru0947@gmail.com>
2025-05-26 17:05:37 +0900

    Section14：ToDoの一覧表示練習問題

--- client/src/components/pages/Top/index.jsx
+++ client/src/components/pages/Top/index.jsx
@@ -1,11 +1,46 @@
+import { useState, useEffect } from 'react'
+import { axios } from '../../../utils/axiosConfig'
+
 import { Layout } from '../../ui/Layout'
+import { ListItem } from '../../ui/ListItem'
+import { Button } from '../../ui/Button'
+import { Icon } from '../../ui/Icon'
 
 import styles from './index.module.css'
 
 export const Top = () => {
+  const[todos,setTodos] = useState([])
+  const [inputValues,setInputValues] = useState({
+    title: '',
+    description: '',
+  })
+  const [isAddTaskFormOpen, setIsAddTaskFormOpen] = useState(false)
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/todo') .then(({data})=> {
+      console.log(data)
+      setTodos(data)
+    })
+  }, [])
   return (
     <Layout>
       <h1 className={styles.heading}>ToDo一覧</h1>
+      <ul className={styles.list}>
+        {todos.map((todo) => {
+          return <ListItem key={todo.id} todo={todo}/>
+        })}
+        <li>
+          <Button buttonStyle='indigo-blue' className={styles['add-task']}>
+            <Icon
+            iconName='plus'
+            color='orange'
+            size='medium'
+            className={styles['plus-icon']}
+          />
+          タスクを追加
+          </Button>
+        </li>
+      </ul>
     </Layout>
   )
 }
