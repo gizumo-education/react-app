8aedb7816a51670823c26f5b5c9d4014fe0369eb
Hyogo03 <sasahyogo@gmail.com>
2025-06-28 19:52:59 +0900

    16.ToDoの編集

--- client/src/components/pages/Top/index.jsx
+++ client/src/components/pages/Top/index.jsx
@@ -12,6 +12,7 @@ import { Form } from '../../ui/Form'
 
 export const Top = () => {
   const [todos, setTodos] = useState([])
+  const [editTodoId, setEditTodoId] = useState('')
   const [inputValues, setInputValues] = useState({
     title: '',
     description: '',
@@ -19,10 +20,13 @@ export const Top = () => {
   const [isAddTaskFormOpen, setIsAddTaskFormOpen] = useState(false)
 
   const handleAddTaskButtonClick = useCallback(() => {
+    setInputValues({ title: '', description: '' })
+    setEditTodoId('')
     setIsAddTaskFormOpen(true)
   }, [])
 
   const handleCancelButtonClick = useCallback(() => {
+    setEditTodoId('')
     setIsAddTaskFormOpen(false)
   }, [])
 
@@ -31,12 +35,13 @@ export const Top = () => {
     setInputValues((prev) => ({ ...prev, [name]: value }))
   }, [])
 
+  // タスク追加
   const handleCreateTodoSubmit = useCallback(
     (event) => {
       event.preventDefault()
       axios.post('http://localhost:3000/todo', inputValues).then(({ data }) => {
         console.log(data)
-        setTodos((prevTodos) => [...prevTodos, data]);
+        setTodos(prevTodos => [...prevTodos, data]);
         setIsAddTaskFormOpen(false)
         setInputValues({
           title:'',
@@ -46,6 +51,35 @@ export const Top = () => {
     },
     [inputValues]
   )
+
+  // タスク編集
+  const handleEditedTodoSubmit = useCallback(
+    (event) => {
+      event.preventDefault()
+      axios
+        .patch(`http://localhost:3000/todo/${editTodoId}`, inputValues)
+        .then(({ data }) => {
+          console.log(data)
+          setTodos(prevTodos => 
+            prevTodos.map(todo => 
+              (todo.id === editTodoId ? data : todo)
+            )
+          )
+          setEditTodoId('')
+        })
+    },
+    [editTodoId, inputValues]
+  )
+
+  const handleEditButtonClick = useCallback((id) => {
+    setIsAddTaskFormOpen(false)
+    setEditTodoId(id)
+    const targetTodo = todos.find((todo) => todo.id === id)
+    setInputValues({
+      title: targetTodo.title,
+      description: targetTodo.description,
+    })
+  }, [todos])
 
   useEffect(() => {
     axios.get('http://localhost:3000/todo').then(({ data }) => {
@@ -59,7 +93,27 @@ export const Top = () => {
       <h1 className={styles.heading}>ToDo一覧</h1>
       <ul className={styles.list}>
         {todos.map((todo) => {
-          return <ListItem key={todo.id} todo={todo} />
+          // idが一致する場合は編集フォームを表示する
+          if (editTodoId === todo.id) {
+            return (
+              <li key={todo.id}>
+                <Form
+                  value={inputValues}
+                  editTodoId={editTodoId}
+                  onChange={handleInputChange}
+                  onCancelClick={handleCancelButtonClick}
+                  onSubmit={handleEditedTodoSubmit}
+                />
+              </li>
+            )
+          }
+          return (
+            <ListItem
+              key={todo.id}
+              todo={todo} 
+              onEditButtonClick={handleEditButtonClick}
+            />
+          )
         })}
         <li>
           {isAddTaskFormOpen ? (
--- client/src/components/ui/ListItem/index.jsx
+++ client/src/components/ui/ListItem/index.jsx
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types'
 import { memo } from 'react'
-
+import { Button } from '../Button'
+import { Icon } from '../Icon'
 import styles from './index.module.css'
 
-export const ListItem = memo(({ todo }) => {
+export const ListItem = memo(({ todo, onEditButtonClick }) => {
   return (
     <li className={styles['list-item']}>
       <div className={styles.task}>
@@ -12,6 +13,14 @@ export const ListItem = memo(({ todo }) => {
           <div className={styles.description}>{todo.description}</div>
         )}
       </div>
+      <div className={styles['task-action']}>
+      <Button
+        buttonStyle='icon-only'
+        onClick={() => onEditButtonClick(todo.id)}
+      >
+        <Icon iconName='edit' color='indigo-blue' size='medium' />
+      </Button>
+    </div>
     </li>
   )
 })
@@ -24,4 +33,5 @@ ListItem.propTypes = {
     description: PropTypes.string,
     isCompleted: PropTypes.bool.isRequired,
   }).isRequired,
+  onEditButtonClick: PropTypes.func.isRequired,
 }
