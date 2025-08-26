b7226530d4b45df73f761fcfd08aa01fc553ff11
濵田沙羅 <sarada20020628@gmail.com>
2025-08-26 18:27:47 +0900

    section16のToDoの編集機能を追加

--- client/src/components/pages/Top/index.jsx
+++ client/src/components/pages/Top/index.jsx
@@ -8,9 +8,12 @@ import { Icon } from '../../ui/Icon'
 
 import styles from './index.module.css'
 import { Form } from '../../ui/Form'
+import { BiEdit } from 'react-icons/bi'
 
 export const Top = () => {
   const [todos, setTodos] = useState([])
+
+  const [editTodoId, setEditTodoId] = useState('')
 
   const [inputValues, setInputValues] = useState({
     title: '',
@@ -19,10 +22,13 @@ export const Top = () => {
   const [isAddTaskFormOpen, setIsAddTaskFormOpen] = useState(false)
 
   const handleAddTaskButtonClick = useCallback(() => {
+    setInputValues({title: '', description: ''})
+    setEditTodoId('')
     setIsAddTaskFormOpen(true)
   }, [])
 
   const handleCancelButtonClick = useCallback(() => {
+    setEditTodoId('')
     setIsAddTaskFormOpen(false)
   }, [])
 
@@ -51,44 +57,93 @@ export const Top = () => {
     [inputValues]
   )
 
-  useEffect(() => {
-    axios.get('http://localhost:3000/todo').then(({data}) => {
-      // console.log(data)
-      setTodos(data)
-    })
-  }, [])
+  const handleEditedTodoSubmit = useCallback(
+    (event) => {
+      event.preventDefault()
+      axios
+        .patch(`http://localhost:3000/todo/${editTodoId}`, inputValues)
+        .then(({data}) => {
+          // 1 更新されたtodoを一覧に表示
+          setTodos((prevTodos) =>
+            prevTodos.map((todo) =>
+              todo.id === editTodoId ? data : todo
+            )
+          )
 
-  return (
-    <Layout>
-      <h1 className={styles.heading}>ToDo一覧</h1>
-      <ul className={styles.list}>
-        {todos.map((todo) => {
-          return <ListItem key={todo.id} todo={todo}/>
-        })}
-
-        <li>
-          {isAddTaskFormOpen ? (
-            <Form value={inputValues}
-                  onCancelClick={handleCancelButtonClick}
-                  onChange={handleInputChange}
-                  onSubmit={handleCreateTodoSubmit}
-            />
-          ) : (
-            <Button
-              buttonStyle='indigo-blue'
-              onClick={handleAddTaskButtonClick}
-              className={styles['add-task']}>
-              <Icon
-                iconName='plus'
-                color='orange'
-                size='medium'
-                className={styles['plus-icon']}
-              />
-                タスクを追加
-            </Button>
-          )}
-        </li>
-      </ul>
-    </Layout>
+          // 2 編集フォームを非表示
+          setEditTodoId('')
+        }).catch((error) => {
+          console.log('Todoの編集に失敗しました')
+        })
+    },
+    [editTodoId, inputValues]
   )
+
+  const handleEditButtonClick = useCallback((id) => {
+    setIsAddTaskFormOpen(false)
+    setEditTodoId(id)
+
+    const targetTodo = todos.find((todo) => todo.id === id)
+    setInputValues({
+      title: targetTodo.title,
+      description: targetTodo.description,
+    })
+  },
+  [todos]
+  )
+
+    useEffect(() => {
+      axios.get('http://localhost:3000/todo').then(({data}) => {
+        // console.log(data)
+        setTodos(data)
+      })
+    }, [])
+
+    return (
+      <Layout>
+        <h1 className={styles.heading}>ToDo一覧</h1>
+        <ul className={styles.list}>
+          {todos.map((todo) => {
+            if (editTodoId === todo.id) {
+              return (
+                <li key={todo.id}>
+                  <Form
+                    value={inputValues}
+                    editTodoId={editTodoId}
+                    onChange={handleInputChange}
+                    onCancelClick={handleCancelButtonClick}
+                    onSubmit={handleEditedTodoSubmit}
+                  />
+                </li>
+              )
+            }
+
+            return <ListItem key={todo.id} todo={todo} onEditButtonClick={handleEditButtonClick}/>
+          })}
+
+          <li>
+            {isAddTaskFormOpen ? (
+              <Form value={inputValues}
+                    onCancelClick={handleCancelButtonClick}
+                    onChange={handleInputChange}
+                    onSubmit={handleCreateTodoSubmit}
+              />
+            ) : (
+              <Button
+                buttonStyle='indigo-blue'
+                onClick={handleAddTaskButtonClick}
+                className={styles['add-task']}>
+                <Icon
+                  iconName='plus'
+                  color='orange'
+                  size='medium'
+                  className={styles['plus-icon']}
+                />
+                  タスクを追加
+              </Button>
+            )}
+          </li>
+        </ul>
+      </Layout>
+    )
 }
