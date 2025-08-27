e0918537da5a7ccc4bb394cebb94234b498b9e46
濵田沙羅 <sarada20020628@gmail.com>
2025-08-27 18:16:30 +0900

    section20のToDoアプリをSPA化、追加

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,15 @@
 import ReactDOM from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import { Top } from './components/pages/Top'
+import { router } from './routes'
 import './styles/reset.css'
 import './styles/globals.css'
+import { Top } from './components/pages/Top'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
     <ToastContainer />
-    <Top />
+    <RouterProvider router={router}/>
   </>
 )
