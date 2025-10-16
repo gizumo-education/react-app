fd135e94c7532b8f9e5cc3cbab1a59078bc7be60
janjan519 <207494690+janjan519@users.noreply.github.com>
2025-10-16 14:09:25 +0900

    feat:ToDoの一覧を表示

--- client/src/components/pages/Top/index.jsx
+++ client/src/components/pages/Top/index.jsx
@@ -1,11 +1,28 @@
+import { useEffect, useState } from 'react'
+import { axios } from '../../../utils/axiosConfig'
+
 import { Layout } from '../../ui/Layout'
 
 import styles from './index.module.css'
+import { ListItem } from '../../ui/ListItem'
 
 export const Top = () => {
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/todo').then(({ data }) => {
+      setTodos(data)
+    })
+  }, [])
+
   return (
     <Layout>
       <h1 className={styles.heading}>ToDo一覧</h1>
+      <ul className={styles.list}>
+        {todos.map((todo) => {
+          return <ListItem key={todo.id} todo={todo} />
+        })}
+      </ul>
     </Layout>
   )
 }
