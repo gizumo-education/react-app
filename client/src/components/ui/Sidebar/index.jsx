d4256ab4580b75e505effb1917ba761f2b7ebb03
takahashi tomasu <takahashitomasu@gmail.com>
2025-10-28 10:25:40 +0900

    ToDoアプリをSPA化

--- client/src/components/ui/Sidebar/index.jsx
+++ client/src/components/ui/Sidebar/index.jsx
@@ -1,26 +1,60 @@
 import { memo } from 'react'
+import { NavLink } from 'react-router-dom'
+
+import { Icon } from '../Icon'
+
 import styles from './index.module.css'
 
 const NaviList = [
   {
     id: 1,
-    lineName: 'ToDo一覧',
-    path: '/top',
+    lineName: 'すべて',
+    path: '/',
+    iconName: 'todoList',
+  },
+  {
+    id: 2,
+    lineName: '完了済み',
+    path: '/completed',
+    iconName: 'completeList',
   },
 ]
 
 export const Sidebar = memo(() => {
   return (
     <ul className={styles.sidebar}>
-      {NaviList.map((list) => {
-        return (
-          <li className={styles.list} key={list.id}>
-            <a href='/' className={styles['navi-item']}>
-              {list.lineName}
-            </a>
-          </li>
-        )
-      })}
+      <li className={['sidebar-list']}>
+        <p className={styles.category}>ToDo</p>
+        <ul className={styles.list}>
+          {NaviList.map((list) => {
+            return (
+              <li className={styles['list-item']} key={list.id}>
+                <NavLink
+                  to={list.path}
+                  end
+                  className={({ isActive }) => {
+                    return isActive
+                      ? `${styles['navi-item']} ${styles.active}`
+                      : [styles['navi-item']]
+                  }}
+                >
+                  {({ isActive }) => (
+                    <>
+                      <Icon
+                        iconName={list.iconName}
+                        color={isActive ? 'dark-blue' : 'indigo-blue'}
+                        size='medium'
+                        className={styles.icon}
+                      />
+                      <span className={styles.text}>{list.lineName}</span>
+                    </>
+                  )}
+                </NavLink>
+              </li>
+            )
+          })}
+        </ul>
+      </li>
     </ul>
   )
 })
