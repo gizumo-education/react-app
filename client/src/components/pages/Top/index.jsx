9e7cb8513207c55aaaca20ce68f83558d65c7f6d
takahashi tomasu <takahashitomasu@gmail.com>
2025-10-29 10:39:48 +0900

    ToDoアプリに完了済み一覧表示

--- client/src/components/pages/Top/index.jsx
+++ client/src/components/pages/Top/index.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect, useCallback } from 'react' 
+import { useRecoilValue, useSetRecoilState } from 'recoil'
 import { axios } from '../../../utils/axiosConfig'
+import { todoState, incompleteTodoListState } from '../../../stores/todoState' 
 import { Layout } from '../../ui/Layout'
 import { ListItem } from '../../ui/ListItem'
 import { Button } from '../../ui/Button' 
@@ -10,7 +12,8 @@ import { errorToast } from '../../../utils/errorToast'
 import styles from './index.module.css'
 
 export const Top = () => {
-  const [todos, setTodos] = useState([])
+  const todos = useRecoilValue(incompleteTodoListState)
+  const setTodos = useSetRecoilState(todoState)
   const [editTodoId, setEditTodoId] = useState('') 
 
   const [inputValues, setInputValues] = useState({
@@ -30,7 +33,6 @@ export const Top = () => {
     setIsAddTaskFormOpen(false)
   }, [])
 
-  // ↓ 追加
   const handleInputChange = useCallback((event) => {
     const { name, value } = event.target
     setInputValues((prev) => ({ ...prev, [name]: value }))
@@ -48,15 +50,15 @@ export const Top = () => {
         errorToast(error.message)
       })
     },
-    [inputValues]
+    [setTodos, inputValues]
   )
 
   const handleEditedTodoSubmit = useCallback(
     (event) => {
       event.preventDefault()
       axios
-        // .patch(`http://localhost:3000/todo/${editTodoId}`, inputValues)
-        .patch(`http://localhost:3000/todo/editTodoId`, inputValues)
+        .patch(`http://localhost:3000/todo/${editTodoId}`, inputValues)
+        // .patch(`http://localhost:3000/todo/editTodoId`, inputValues)
         .then(({ data }) => {
           setTodos(prev => prev.map(todo => 
             todo.id === editTodoId ? data : todo
@@ -77,7 +79,7 @@ export const Top = () => {
           }
         })
     },
-    [editTodoId, inputValues]
+    [setTodos, editTodoId, inputValues]
   )
 
 
@@ -112,7 +114,9 @@ export const Top = () => {
           break
       }
     })
-  }, [])
+  }, 
+  [setTodos]
+  )
 
   const handleToggleButtonClick = useCallback(
     (id) => {
@@ -138,7 +142,7 @@ export const Top = () => {
           }
         })
     },
-    [todos]
+    [todos, setTodos]
   )
 
 
@@ -149,7 +153,7 @@ export const Top = () => {
     .catch((error) => {
       errorToast(error.message)
     })
-  }, [])
+  }, [setTodos])
 
   return (
     <Layout>
