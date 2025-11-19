ea25647081452fc60a7a2ebf8f88b4b374586b3d
yuuna <yuuna@yuunanoMacBook-Pro.local>
2025-11-19 18:50:01 +0900

    push:section14の回答記載

--- client/src/components/pages/Top/index.jsx
+++ client/src/components/pages/Top/index.jsx
@@ -1,11 +1,32 @@
+
+import { useState, useEffect } from 'react' // useStateを追加
+import { axios } from '../../../utils/axiosConfig'
+
 import { Layout } from '../../ui/Layout'
+import { ListItem } from '../../ui/ListItem' // 追加
 
 import styles from './index.module.css'
 
 export const Top = () => {
+  const [todos, setTodos] = useState([]) // 追加
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/todo').then(({ data }) => {
+      console.log(data)
+      setTodos(data)
+    })
+  }, [])
+
   return (
     <Layout>
       <h1 className={styles.heading}>ToDo一覧</h1>
+      {/* // ↓ 追加 */}
+      <ul className={styles.list}>
+        {todos.map((data) => {
+          return <ListItem key={data.id} todo={data} />
+        })}
+      </ul>
+      {/* // ↑ 追加 */}
     </Layout>
   )
 }
