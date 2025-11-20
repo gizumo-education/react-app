777f9daaf937e7f0b7eefdce6f0112ed7248a127
yuuna <yuuna@yuunanoMacBook-Pro.local>
2025-11-20 15:36:30 +0900

    push:section15の回答記載

--- client/src/components/pages/Top/index.jsx
+++ client/src/components/pages/Top/index.jsx
@@ -1,14 +1,47 @@
 
-import { useState, useEffect } from 'react' // useStateを追加
+import { useState, useEffect , useCallback } from 'react' 
 import { axios } from '../../../utils/axiosConfig'
 
 import { Layout } from '../../ui/Layout'
-import { ListItem } from '../../ui/ListItem' // 追加
+import { ListItem } from '../../ui/ListItem' 
+import { Button } from '../../ui/Button' 
+import { Icon } from '../../ui/Icon' 
+import { Form } from '../../ui/Form' 
 
 import styles from './index.module.css'
 
 export const Top = () => {
-  const [todos, setTodos] = useState([]) // 追加
+  const [todos, setTodos] = useState([]) 
+  // ToDoの追加フォームに入力された値を保持
+  const [inputValues, setInputValues] = useState({
+    title: '',
+    description: '',
+  })
+  const handleInputChange = useCallback((event) => {
+    const { name, value } = event.target
+    setInputValues((prev) => ({ ...prev, [name]: value }))
+  }, [])
+  const handleCreateTodoSubmit = useCallback(
+    (event) => {
+      event.preventDefault()
+      axios.post('http://localhost:3000/todo', inputValues).then(({ data }) => {
+        console.log(data)
+        setTodos((newData) => [...newData, data])
+        handleCancelButtonClick()//グローバル関数
+        setInputValues('')
+        // setInputValues({title: '',description: '',})
+      })
+    },
+    [inputValues]
+  )
+  // ToDoの追加フォームの表示・非表示の管理
+  const [isAddTaskFormOpen, setIsAddTaskFormOpen] = useState(false)
+  const handleAddTaskButtonClick = useCallback(() => {
+    setIsAddTaskFormOpen(true)
+  }, [])
+  const handleCancelButtonClick = useCallback(() => {
+    setIsAddTaskFormOpen(false)
+  }, [])
 
   useEffect(() => {
     axios.get('http://localhost:3000/todo').then(({ data }) => {
@@ -16,17 +49,41 @@ export const Top = () => {
       setTodos(data)
     })
   }, [])
+  
 
   return (
     <Layout>
       <h1 className={styles.heading}>ToDo一覧</h1>
-      {/* // ↓ 追加 */}
       <ul className={styles.list}>
+        {/* リストの情報を表示する繰り返し処理 */}
         {todos.map((data) => {
           return <ListItem key={data.id} todo={data} />
         })}
+        {/* ToDoの追加フォームの表示・非表示 */}
+        <li>
+          {isAddTaskFormOpen ? (
+            <Form 
+            value={inputValues} 
+            onChange={handleInputChange}// 追加機能の実装
+            onCancelClick={handleCancelButtonClick}
+            onSubmit={handleCreateTodoSubmit}
+            />
+          ) : (
+            <Button 
+              buttonStyle='indigo-blue' 
+              onClick={handleAddTaskButtonClick}
+              className={styles['add-task']}>
+              <Icon
+                iconName='plus'
+                color='orange'
+                size='medium'
+                className={styles['plus-icon']}
+              />
+              タスクを追加
+            </Button>
+          )}
+        </li>
       </ul>
-      {/* // ↑ 追加 */}
     </Layout>
   )
 }
