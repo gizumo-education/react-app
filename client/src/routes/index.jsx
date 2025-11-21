15d55bc1e5eb0059798f32bb3b1e81763e26f492
新井　路花 <arahana@araijihananoMacBook-Air.local>
2025-11-21 17:33:15 +0900

    section20 完了済みページの遷移実装

---
+++ client/src/routes/index.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from 'react-router-dom'
+
+import { Top } from '../components/pages/Top'
+import { Completed } from '../components/pages/Completed'
+
+export const router = createBrowserRouter([
+  { path: '/', element: <Top /> },
+
+  //section20 練習問題 オブジェクト追加
+  { path: '/completed', element: <Completed /> }
+])
