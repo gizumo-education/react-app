f33fa0ba5e0e3410e3e1ca6afe387e44ad4d246d
shinmoto <shinmoto@gizumo-admin-44noMacBook-Air.local>
2026-01-15 18:58:58 +0900

    ToDoの一覧表示機能を実装

--- client/src/components/pages/Top/index.jsx
+++ client/src/components/pages/Top/index.jsx
@@ -1,11 +1,29 @@
+import { useState, useEffect } from 'react'
+import { axios } from '../../../utils/axiosConfig'
+
 import { Layout } from '../../ui/Layout'
+import { ListItem } from '../../ui/ListItem'
 
 import styles from './index.module.css'
 
 export const Top = () => {
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/todo').then(({ data }) => {
+      console.log(data)
+      setTodos(data)
+    })
+  }, [])
+
   return (
     <Layout>
       <h1 className={styles.heading}>ToDo一覧</h1>
+      <ul className={styles.list}>
+        {todos.map((todo) => {
+          return <ListItem key={todo.id} todo={todo} />
+        })}
+      </ul>
     </Layout>
   )
 }
