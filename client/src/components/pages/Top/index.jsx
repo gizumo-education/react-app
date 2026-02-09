b1faa3b27847c76a561227de56dc97d8e60081e0
taikifujino0511 <taiki.fujino@giztech-pro.com>
2026-02-09 12:07:20 +0900

    Add Section 14 ToDoの一覧表示

--- client/src/components/pages/Top/index.jsx
+++ client/src/components/pages/Top/index.jsx
@@ -1,11 +1,30 @@
+import { useEffect, useState } from 'react'
+import { axios } from '../../../utils/axiosConfig'
+
 import { Layout } from '../../ui/Layout'
+
+import { ListItem } from '../../ui/ListItem'
 
 import styles from './index.module.css'
 
+
 export const Top = () => {
+  const [todos, setTodos] = useState([])
+  // 以下のuseEffectの処理を追加
+  useEffect(() => {
+    axios.get('http://localhost:3000/todo').then(({ data }) => {
+      setTodos(data)
+    })
+  }, [])
+
   return (
     <Layout>
       <h1 className={styles.heading}>ToDo一覧</h1>
+      <ul className={styles.list}>
+        {todos.map((todo) => {
+          return <ListItem key={todo.id} todo={todo} />
+        })}
+      </ul>
     </Layout>
   )
 }
