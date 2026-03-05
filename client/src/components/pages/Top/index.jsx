3835dacfb964b78639640c8dc7c63315f3a8bb81
加藤花実 <hanami@katoukajitsunoMacBook-Air.local>
2026-03-05 15:10:39 +0900

    Section14~16の回答を追加（ToDo一覧表示、ToDo追加機能、編集機能を実装）

--- client/src/components/pages/Top/index.jsx
+++ client/src/components/pages/Top/index.jsx
@@ -1,11 +1,150 @@
+import { useState, useEffect, useCallback } from 'react'
+import { axios } from '../../../utils/axiosConfig'
+
 import { Layout } from '../../ui/Layout'
+import { ListItem } from '../../ui/ListItem'
+import { Button } from '../../ui/Button'
+import { Icon } from '../../ui/Icon'
+import { Form } from '../../ui/Form'
 
 import styles from './index.module.css'
 
 export const Top = () => {
+  const [todos, setTodos] = useState([])
+  const [editTodoId, setEditTodoId] = useState('')
+
+  const [inputValues, setInputValues] = useState({
+    title: '',
+    description: '',
+  })
+  const [isAddTaskFormOpen, setIsAddTaskFormOpen] = useState(false)
+
+  const handleAddTaskButtonClick = useCallback(() => {
+    setInputValues({ title: '', description: '' })
+    setEditTodoId('')
+    setIsAddTaskFormOpen(true)
+  }, [])
+
+  const handleCancelButtonClick = useCallback(() => {
+    setEditTodoId('')
+    setIsAddTaskFormOpen(false)
+  }, [])
+
+  const handleInputChange = useCallback((event) => {
+    const { name, value } = event.target
+    setInputValues((prev) => ({ ...prev, [name]: value }))
+  }, [])
+
+  const handleCreateTodoSubmit = useCallback(
+    (event) => {
+      event.preventDefault()
+      axios.post('http://localhost:3000/todo', inputValues).then(({ data }) => {
+        console.log(data)
+        setTodos([...todos, data])//Section15 問題1で追加。
+        setIsAddTaskFormOpen(false)//Section15 問題2で追加。
+        setInputValues({           //Section15 問題3で追加。
+          title: '',
+          description: '',
+        })
+      })
+    },
+    [inputValues]
+  )
+
+  const handleEditedTodoSubmit = useCallback(
+    (event) => {
+      event.preventDefault()
+      axios
+        .patch(`http://localhost:3000/todo/${editTodoId}`, inputValues)
+        .then(({ data }) => {
+          console.log(data)
+
+          setTodos(               //Section16　問題1で追加
+          todos.map((todo) => {
+    if (todo.id === editTodoId) {
+      return data
+    }
+    return todo
+  })
+        )
+        setEditTodoId('')         //Section16　問題2で追加
+
+        })
+    },
+    [editTodoId, inputValues]
+  )
+
+  const handleEditButtonClick = useCallback(
+    (id) => {
+    setIsAddTaskFormOpen(false)
+    setEditTodoId(id)
+    const targetTodo = todos.find((todo) => todo.id === id)
+    setInputValues({
+      title: targetTodo.title,
+      description: targetTodo.description,
+    })
+  },
+  [todos]
+)
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/todo').then(({ data }) => {
+      console.log(data)
+      setTodos(data)    //Section14で追加
+    })
+  }, [])
+
   return (
     <Layout>
       <h1 className={styles.heading}>ToDo一覧</h1>
+      <ul className={styles.list}>
+        {todos.map((todo) => {
+          if (editTodoId === todo.id) {
+            return (
+              <li key={todo.id}>
+                <Form
+                  value={inputValues}
+                  editTodoId={editTodoId}
+                  onChange={handleInputChange}
+                  onCancelClick={handleCancelButtonClick}
+                  onSubmit={handleEditedTodoSubmit}
+                />
+              </li>
+            )
+          }
+          return (
+            <ListItem
+              key={todo.id}
+              todo={todo}
+              onEditButtonClick={handleEditButtonClick}
+            />
+          )
+        })}
+        <li>
+          {isAddTaskFormOpen ? (
+            <Form
+              value={inputValues}
+              onChange={handleInputChange}
+              onCancelClick={handleCancelButtonClick}
+              onSubmit={handleCreateTodoSubmit}
+            />
+          ) : (
+            <Button
+              buttonStyle='indigo-blue'
+              onClick={handleAddTaskButtonClick}
+              className={styles['add-task']}
+            >
+              <Icon
+                iconName='plus'
+                color='orange'
+                size='medium'
+                className={styles['plus-icon']}
+              />
+              タスクを追加
+            </Button>
+          )}
+        </li>
+      </ul>
     </Layout>
   )
 }
