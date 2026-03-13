3d1501bd37684ccdc0cb0e899555effdb6966404
加藤花実 <hanami@katoukajitsunoMacBook-Air.local>
2026-03-13 14:17:57 +0900

    Completed/index.jsx　完了状態切り替え処理の不足コードを追加。完了済みToDoの表示処理を修正

--- client/src/components/pages/Completed/index.jsx
+++ client/src/components/pages/Completed/index.jsx
@@ -5,27 +5,39 @@ import { ListItem } from "../../ui/ListItem";
 import styles from './index.module.css'
 
 import { axios } from '../../../utils/axiosConfig'
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import { useRecoilValue, useSetRecoilState } from "recoil";
 import { errorToast } from "../../../utils/errorToast";
 
 export const Completed = () => {
   const todos = useRecoilValue(completedTodoListState)
   const setTodos = useSetRecoilState(todoState)
-  const handleToggleButtonClick = (id) => {
-    axios
-      .patch(`http://localhost:3000/todo/${id}/completion-status`, {
-        isCompleted: todos.find((todo) => todo.id === id).isCompleted,
-      })
-      .then(({ data }) => {
-        setTodos((prev) =>
-          prev.map((todo) => (todo.id === id ? data : todo))
-        )
-      })
-      .catch((error) => {
-        errorToast(error.message)
-      })
-  }
+  const handleToggleButtonClick = useCallback(
+    (id) => {
+      axios
+        .patch(`http://localhost:3000/todo/${id}/completion-status`, {
+          isCompleted: todos.find((todo) => todo.id === id).isCompleted,
+        })
+        .then(({ data }) => {
+          setTodos((prev) =>
+            prev.map((todo) => (todo.id === data.id ? data : todo))
+          )
+        })
+        .catch((error) => {
+          switch (error.statusCode) {
+            case 404:
+              errorToast(
+                '完了・未完了を切り替えるToDoが見つかりませんでした。画面を更新して再度お試しください。'
+              )
+              break
+            default:
+              errorToast(error.message)
+              break
+          }
+        })
+    },
+    [todos, setTodos]
+  )
 
 
   useEffect(() => {
@@ -41,18 +53,19 @@ export const Completed = () => {
   return (
     <Layout>
       <h1 className={styles.heading}>完了済み一覧</h1>
-      <ul>
-        {todos.length === 0 ? (
-          <li>完了済みのToDoはありません。</li>
-        ) : (
-          todos.map((todo) => (
-            <ListItem key={todo.id}
+      {todos.length ? (
+        <ul className={styles.list}>
+          {todos.map((todo) => (
+            <ListItem
+              key={todo.id}
               todo={todo}
               onToggleButtonClick={handleToggleButtonClick}
             />
-          ))
-        )}
-      </ul>
+          ))}
+        </ul>
+      ) : (
+        <p className={styles['no-todo']}>完了済みのToDoはありません。</p>
+      )}
     </Layout>
   )
 }
