4dfbee1d90e6170383e1daa57153a3101197816a
ren <sk93txnm1582@gmail.com>
2026-03-25 12:52:17 +0900

    section14コード追加

--- client/src/components/pages/Top/index.jsx
+++ client/src/components/pages/Top/index.jsx
@@ -1,11 +1,33 @@
+
+import { useState, useEffect } from 'react' // 追加
+import { axios } from '../../../utils/axiosConfig' // 追加
+
 import { Layout } from '../../ui/Layout'
+import { ListItem } from '../../ui/ListItem' // 追加
 
 import styles from './index.module.css'
 
 export const Top = () => {
+  const [todos, setTodos] = useState([]) // 追加
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/todo').then(({ data }) => {
+      console.log(date);
+      
+    })
+  }, [])
+
   return (
     <Layout>
-      <h1 className={styles.heading}>ToDo一覧</h1>
+      <h1 className={styles.heading}>ToDo一覧
+              // ↓ 追加
+      <ul className={styles.list}>
+        {todos.map((todo) => {
+          return <ListItem key={todo.id} todo={todo} />
+        })}
+      </ul>
+      // ↑ 追加
+      </h1>
     </Layout>
   )
 }
