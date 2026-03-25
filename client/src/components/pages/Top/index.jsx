91cf2f0bf547c7f369f164ba7777636878a30e8f
ren <sk93txnm1582@gmail.com>
2026-03-25 18:33:50 +0900

    Todoの追加

--- client/src/components/pages/Top/index.jsx
+++ client/src/components/pages/Top/index.jsx
@@ -1,15 +1,63 @@
 // Top/index.jsx
 
-import { useState, useEffect } from 'react' // useStateを追加
+import { useState, useEffect, useCallback } from 'react' // useStateを追加
 import { axios } from '../../../utils/axiosConfig'
 
 import { Layout } from '../../ui/Layout'
 import { ListItem } from '../../ui/ListItem' // 追加
+import { Button } from '../../ui/Button' // 追加
+import { Icon } from '../../ui/Icon' // 追加
+import { Form } from '../../ui/Form' // 追加
 
 import styles from './index.module.css'
 
 export const Top = () => {
   const [todos, setTodos] = useState([]) // 追加
+  const [inputValues, setInputValues] = useState({
+    title: '',
+    description: '',
+  })
+
+  const [isAddTaskFormOpen, setIsAddTaskFormOpen] = useState(false) // 追加
+
+  const handleAddTaskButtonClick = useCallback(() => {
+    setIsAddTaskFormOpen(true)
+  }, [])
+
+  const handleCancelButtonClick = useCallback(() => {
+    setIsAddTaskFormOpen(false)
+  }, [])
+
+  // ↓ 追加
+  const handleCreateTodoSubmit = useCallback(
+    (event) => {
+      event.preventDefault()
+      axios.post('http://localhost:3000/todo', inputValues).then(({ data }) => {
+        console.log(data)
+
+        // ① 追加したToDoが一覧に表示される
+        setTodos((prev) => [...prev, data])
+
+        // ② ToDoの追加フォームを非表示
+        setIsAddTaskFormOpen(false)
+
+        // ③ ToDoの追加フォームの入力欄を空にする
+        setInputValues({
+          title: '',
+          description: '',
+        })
+      })
+    },
+    [inputValues]
+  )
+  // ↑ 追加
+
+  // ↓ 追加
+  const handleInputChange = useCallback((event) => {
+    const { name, value } = event.target
+    setInputValues((prev) => ({ ...prev, [name]: value }))
+  }, [])
+  // ↑ 追加
 
   useEffect(() => {
     axios.get('http://localhost:3000/todo').then(({ data }) => {
@@ -25,6 +73,30 @@ export const Top = () => {
         {todos.map((todo) => {
           return <ListItem key={todo.id} todo={todo} />
         })}
+        <li>
+
+          {isAddTaskFormOpen ? (
+            <Form
+              value={inputValues}
+              onChange={handleInputChange}
+              onCancelClick={handleCancelButtonClick}
+              onSubmit={handleCreateTodoSubmit}
+            />
+          ) : (
+            <Button
+              buttonStyle='indigo-blue'
+              onClick={handleAddTaskButtonClick} // 追加
+              className={styles['add-task']}>
+              <Icon
+                iconName='plus'
+                color='orange'
+                size='medium'
+                className={styles['plus-icon']}
+              />
+              タスクを追加
+            </Button>
+          )}
+        </li>
       </ul>
     </Layout>
   )
---
+++ client/src/components/ui/Form/index.jsx
@@ -0,0 +1,59 @@
+// ui/Form/index.jsx
+
+import PropTypes from 'prop-types'
+import { memo } from 'react'
+import { Button } from '../Button'
+
+import styles from './index.module.css'
+
+export const Form = memo(({ value, onChange, onCancelClick, onSubmit }) => {
+  return (
+    <form onSubmit={onSubmit} className={styles.form}>
+      <div className={styles['input-field']}>
+        <input
+          type='text'
+          name='title'
+          placeholder='タスク名'
+          autoFocus
+          value={value.title}
+          onChange={onChange}
+          className={styles['input-title']}
+        />
+        <textarea
+          name='description'
+          placeholder='説明'
+          value={value.description}
+          onChange={onChange}
+          className={styles['input-description']}
+        />
+      </div>
+      <div className={styles['button-area']}>
+        <Button
+          buttonStyle='cancel'
+          className={styles['cancel-button']}
+          onClick={onCancelClick}
+        >
+          キャンセル
+        </Button>
+        <Button
+          type='submit'
+          disabled={!value.title}
+          className={styles['submit-button']}
+        >
+          タスクを追加
+        </Button>
+      </div>
+    </form>
+  )
+})
+
+Form.displayName = 'Form'
+Form.propTypes = {
+  value: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+  }).isRequired,
+  onChange: PropTypes.func.isRequired,
+  onCancelClick: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+}
