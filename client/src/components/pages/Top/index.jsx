8636a9ae44adfcd5209fe7dbf0f86158c531dcc0
chinami <cynthia.wanwan081505@gmail.com>
2026-04-23 15:09:58 +0900

    firstcommit

--- client/src/components/pages/Top/index.jsx
+++ client/src/components/pages/Top/index.jsx
@@ -1,11 +1,181 @@
+import { useState, useEffect, useCallback } from 'react'
+import { axios } from '../../../utils/axiosConfig'
 import { Layout } from '../../ui/Layout'
-
+import { ListItem } from '../../ui/ListItem'
+import { Button } from '../../ui/Button'
+import { Icon } from '../../ui/Icon'
+import { Form } from '../../ui/Form'
+import { errorToast } from '../../../utils/errorToast'
 import styles from './index.module.css'
 
 export const Top = () => {
+  const [todos, setTodos] = useState([])
+  const [editTodoId, setEditTodoId] = useState('')
+  const [inputValues, setInputValues] = useState({
+    title: '',
+    description: '',
+  })
+  const [isAddTaskFormOpen, setIsAddTaskFormOpen] = useState(false)
+  const handleAddTaskButtonClick = useCallback(() => {
+    setInputValues({ title: '', description: '' })
+    setEditTodoId('')
+    setIsAddTaskFormOpen(true)
+  }, [])
+  const handleCancelButtonClick = useCallback(() => {
+    setEditTodoId('')
+    setIsAddTaskFormOpen(false)
+  }, [])
+  const handleInputChange = useCallback((event) => {
+    const { name, value } = event.target
+    setInputValues((prev) => ({ ...prev, [name]: value }))
+  }, [])
+  const handleCreateTodoSubmit = useCallback(
+    (event) => {
+      event.preventDefault()
+      axios.post('http://localhost:3000/todo', inputValues).then(({ data }) => {
+        setTodos((prev) => [...prev, data])
+        setIsAddTaskFormOpen(false)
+        setInputValues({ title: '', description: '' })
+      })
+        .catch((error) => {
+          errorToast(error.message)
+        })
+    },
+    [inputValues]
+  )
+  const handleEditedTodoSubmit = useCallback(
+    (event) => {
+      event.preventDefault()
+      axios.patch(`http://localhost:3000/todo/${editTodoId}`, inputValues).then(({ data }) => {
+        setTodos((prev) =>
+          prev.map((todo) => (todo.id === editTodoId ? data : todo))
+        );
+        setEditTodoId('');
+        setInputValues({ title: '', description: '' });
+        console.log(data)
+      })
+        .catch((error) => {
+          switch (error.statusCode) {
+            case 404:
+              errorToast(
+                '更新するToDoが見つかりませんでした。画面を更新して再度お試しください。'
+              )
+              break
+            default:
+              errorToast(error.message)
+              break
+          }
+        })
+    },
+    [editTodoId, inputValues]
+  )
+  const handleEditButtonClick = useCallback((id) => {
+    setIsAddTaskFormOpen(false)
+    setEditTodoId(id)
+    const targetTodo = todos.find((todo) => todo.id === id)
+    setInputValues({
+      title: targetTodo.title,
+      description: targetTodo.description,
+    })
+  },
+    [todos]
+  )
+  const handleDeleteButtonClick = useCallback((id) => {
+    axios.delete(`http://localhost:3000/todo/${id}`).then(() => {
+      setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id))
+    })
+      .catch((error) => {
+        switch (error.statusCode) {
+          case 404:
+            errorToast(
+              '削除するToDoが見つかりませんでした。画面を更新して再度お試しください。'
+            )
+            break
+          default:
+            errorToast(error.message)
+            break
+        }
+      })
+  }, [])
+
+  const handleToggleButtonClick = useCallback(
+    (id) => {
+      axios
+        .patch(`http://localhost:3000/todo/${id}/completion-status`, {
+          isCompleted: todos.find((todo) => todo.id === id).isCompleted,
+        })
+        .then(({ data }) => {
+          setTodos((prev) =>
+            prev.map((todo) => (todo.id === id ? data : todo))
+          );
+          console.log(data)
+        })
+        .catch((error) => {
+          switch (error.statusCode) {
+            case 404:
+              errorToast(
+                '完了・未完了を切り替えるToDoが見つかりませんでした。画面を更新して再度お試しください。'
+              )
+              break
+            default:
+              errorToast(error.message)
+              break
+          }
+        })
+    },
+    [todos]
+  )
+  useEffect(() => {
+    axios.get('http://localhost:3000/todo').then(({ data }) => {
+      setTodos(data)
+      console.log(data)
+    })
+      .catch((error) => {
+        errorToast(error.message)
+      })
+  }, [])
   return (
     <Layout>
       <h1 className={styles.heading}>ToDo一覧</h1>
-    </Layout>
+      <ul className={styles.list}>
+        {todos.map((todo) => {
+          if (editTodoId === todo.id) {
+            return (
+              <li key={todo.id}>
+                <Form
+                  value={inputValues}
+                  editTodoId={editTodoId}
+                  onChange={handleInputChange}
+                  onCancelClick={handleCancelButtonClick}
+                  onSubmit={handleEditedTodoSubmit}
+                />
+              </li>
+            )
+          }
+          return <ListItem
+            key={todo.id}
+            todo={todo}
+            onEditButtonClick={handleEditButtonClick}
+            onDeleteButtonClick={handleDeleteButtonClick}
+            onToggleButtonClick={handleToggleButtonClick}
+          />
+        })}
+        <li>
+          {isAddTaskFormOpen ? (
+            <Form value={inputValues} onChange={handleInputChange} onCancelClick={handleCancelButtonClick} onSubmit={handleCreateTodoSubmit} />
+          ) : (
+            <Button buttonStyle='indigo-blue' onClick={handleAddTaskButtonClick} className={styles['add-task']}>
+              <Icon
+                iconName='plus'
+                color='orange'
+                size='medium'
+                className={styles['plus-icon']}
+              />
+              タスクを追加
+            </Button>
+          )}
+        </li>
+      </ul>
+    </Layout >
   )
 }
--- scripts/start_client.sh
+++ scripts/start_client.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-
-PORT=3000
-
-if lsof -Pi :$PORT -sTCP:LISTEN -t >/dev/null; then
-  kill $(lsof -t -i:$PORT)
-fi
-
-while ! nc -z localhost $PORT; do
-  sleep 1
-done
-
 npm run dev -w client
+
+# #!/bin/bash
+
+# PORT=3000
+
+# if lsof -Pi :$PORT -sTCP:LISTEN -t >/dev/null; then
+#   kill $(lsof -t -i:$PORT)
+# fi
+
+# while ! nc -z localhost $PORT; do
+#   sleep 1
+# done
+
+# npm run dev -w client
