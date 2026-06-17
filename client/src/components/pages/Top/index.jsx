1be8a7034ea52c968ecb1056407a45c317922b5b
KaiSaxophone <kai.saxophone@gmail.com>
2026-06-17 12:01:32 +0900

    feat: ToDo一覧表示の実装

--- client/src/components/pages/Top/index.jsx
+++ client/src/components/pages/Top/index.jsx
@@ -1,21 +1,30 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { axios } from '../../../utils/axiosConfig'
 
 import { Layout } from '../../ui/Layout'
+import { ListItem } from '../../ui/ListItem'
 
 import styles from './index.module.css'
 
 export const Top = () => {
+  const [todos, setTodos] = useState([])
+  
   // レンダリング時にToDo一覧を取得
   useEffect(() => {
     axios.get('http://localhost:3000/todo').then(({ data }) => {
-      console.log(data)
+      // console.log(data)
+      setTodos(data)
     })
   }, [])
 
   return (
     <Layout>
       <h1 className={styles.heading}>ToDo一覧</h1>
+      <ul className={styles.list}>
+        {todos.map((todo) => {
+          return <ListItem key={todo.id} todo={todo} />
+        })}
+      </ul>
     </Layout>
   )
 }
