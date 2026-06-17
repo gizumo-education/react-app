9541896c2ad80969885db46c5fad939061bea78f
KaiSaxophone <kai.saxophone@gmail.com>
2026-06-17 11:17:29 +0900

    add: APIでToDo一覧を取得

--- client/src/components/pages/Top/index.jsx
+++ client/src/components/pages/Top/index.jsx
@@ -1,8 +1,18 @@
+import { useEffect } from 'react'
+import { axios } from '../../../utils/axiosConfig'
+
 import { Layout } from '../../ui/Layout'
 
 import styles from './index.module.css'
 
 export const Top = () => {
+  // レンダリング時にToDo一覧を取得
+  useEffect(() => {
+    axios.get('http://localhost:3000/todo').then(({ data }) => {
+      console.log(data)
+    })
+  }, [])
+
   return (
     <Layout>
       <h1 className={styles.heading}>ToDo一覧</h1>
