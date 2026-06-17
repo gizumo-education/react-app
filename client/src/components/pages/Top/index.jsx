800282dee7653fa6870034317ae54f79aa5d7829
KaiSaxophone <kai.saxophone@gmail.com>
2026-06-17 12:54:23 +0900

    add: タスクを追加ボタンopen時の表示の実装

--- client/src/components/pages/Top/index.jsx
+++ client/src/components/pages/Top/index.jsx
@@ -5,6 +5,7 @@ import { Layout } from '../../ui/Layout'
 import { ListItem } from '../../ui/ListItem'
 import { Button } from '../../ui/Button'
 import { Icon } from '../../ui/Icon'
+import { Form } from '../../ui/Form'
 
 import styles from './index.module.css'
 
@@ -14,6 +15,7 @@ export const Top = () => {
     title: '',
     desctiprion: ''
   })
+  const [isAddTaskFormOpen, setIsAddTaskFormOpen] = useState(false)
   
   // レンダリング時にToDo一覧を取得
   useEffect(() => {
@@ -31,16 +33,20 @@ export const Top = () => {
           return <ListItem key={todo.id} todo={todo} />
         })}
         <li>
-        <Button buttonStyle='indigo-blue' className={styles['add-task']}>
-          <Icon
-            iconName='plus'
-            color='orange'
-            size='medium'
-            className={styles['plus-icon']}
-          />
-          タスクを追加
-        </Button>
-      </li>
+          {isAddTaskFormOpen ? (
+            <Form value={inputValues} />
+          ) : (
+            <Button buttonStyle='indigo-blue' className={styles['add-task']}>
+            <Icon
+              iconName='plus'
+              color='orange'
+              size='medium'
+              className={styles['plus-icon']}
+            />
+            タスクを追加
+          </Button>
+          )}
+        </li>
       </ul>
     </Layout>
   )
