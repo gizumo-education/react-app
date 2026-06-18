2055811ed93df8680ebf061b1882f2ae4e9f1107
KaiSaxophone <kai.saxophone@gmail.com>
2026-06-18 17:47:31 +0900

    style: 完了ボタンのstyle追加

--- client/src/components/ui/ListItem/index.jsx
+++ client/src/components/ui/ListItem/index.jsx
@@ -36,9 +36,21 @@ export const ListItem = memo(({ todo, onEditButtonClick, onDeleteButtonClick, on
       </Button>
       )}
       <div className={styles.task}>
-        <div className={styles.title}>{todo.title}</div>
+        <div
+          className={`${styles.title} ${
+            todo.isCompleted ? styles['task-completed'] : ''
+          }`}
+        >
+          {todo.title}
+        </div>
         {todo.description && (
-          <div className={styles.description}>{todo.description}</div>
+          <div
+            className={`${styles.description} ${
+              todo.isCompleted ? styles['task-completed'] : ''
+            }`}
+          >
+            {todo.description}
+          </div>
         )}
       </div>
       <div className={styles['task-action']}>
