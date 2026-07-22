4fe9587eb5835f3299e1bd078c1c51554f243198
saki0506 <172128609+saki0506@users.noreply.github.com>
2026-07-22 11:56:54 +0900

    feat: useState・useEffectでTODO一覧を取得・表示

--- client/src/components/pages/Top/index.jsx
+++ client/src/components/pages/Top/index.jsx
@@ -1,11 +1,29 @@
+import { useEffect, useState } from 'react' // 追加
+import { axios } from '../../../utils/axiosConfig' // 追加
+
 import { Layout } from '../../ui/Layout'
+import { ListItem } from '../../ui/ListItem' // 追加
 
 import styles from './index.module.css'
 
 export const Top = () => {
+  const [todos, setTodos] = useState([]) // 追加
+  // 以下のuseEffectの処理を追加
+  useEffect(() => {
+    axios.get('http://localhost:3000/todo').then(({ data }) => {
+      console.log(data)
+      setTodos(data)
+    })
+  }, [])
+
   return (
     <Layout>
       <h1 className={styles.heading}>ToDo一覧</h1>
+      <ul className={styles.list}>
+        {todos.map((todo) => {
+          return <ListItem key={todo.id} todo={todo} />
+        })}
+      </ul>
     </Layout>
   )
 }
