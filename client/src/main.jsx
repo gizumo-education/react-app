98971ce6c062ca1156e0f91efee8baec0ec25342
saki0506 <172128609+saki0506@users.noreply.github.com>
2026-07-28 16:54:36 +0900

    feat: 完了済み一覧ページのルーティングを追加

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,8 @@
 import ReactDOM from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import { router } from './routes'
 import { Top } from './components/pages/Top'
 import './styles/reset.css'
 import './styles/globals.css'
@@ -8,6 +10,6 @@ import './styles/globals.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
     <ToastContainer />
-    <Top />
+    <RouterProvider router={router} />
   </>
 )
