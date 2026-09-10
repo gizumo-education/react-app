8e2c7a72c746dbd80d6c356107e42ece61e7aabe
machi2255 <132818684+machi2255@users.noreply.github.com>
2026-09-10 10:51:06 +0900

    ToDoの表示

--- client/src/components/pages/Top/index.jsx
+++ client/src/components/pages/Top/index.jsx
@@ -1,11 +1,29 @@
+import { useState, useEffect } from 'react' // useStateを追加
+import { axios } from '../../../utils/axiosConfig'
+
 import { Layout } from '../../ui/Layout'
+import { ListItem } from '../../ui/ListItem' // 追加
 
 import styles from './index.module.css'
 
 export const Top = () => {
+  const [todos, setTodos] = useState([]) // 追加
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/todo').then(({ data }) => {
+      console.log(data)
+      setTodos(data)
+    })
+  }, [])
+
   return (
     <Layout>
       <h1 className={styles.heading}>ToDo一覧</h1>
+      <ul className={styles.list}>
+        {todos.map((todo) => {
+          return <ListItem key={todo.id} todo={todo} />
+        })}
+      </ul>
     </Layout>
   )
 }
