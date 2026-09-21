44918521afe02a8dad13d43faee280842cd0bf07
machi2255 <132818684+machi2255@users.noreply.github.com>
2026-09-21 18:22:31 +0900

    ReactRouter SPA

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,14 @@
 import ReactDOM from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom' // 追加
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import { Top } from './components/pages/Top'
+import { router } from './routes' // Topコンポーネントのimportから変更
 import './styles/reset.css'
 import './styles/globals.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
     <ToastContainer />
-    <Top />
+    <RouterProvider router={router} />
   </>
 )
